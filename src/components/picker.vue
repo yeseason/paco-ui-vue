57f5364eaddc148526164a6871837ac6d92aa9c1
谢瑞卿 <reseason@gmail.com>
2016-11-09 16:28:23 +0800

    代码提交

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -3,21 +3,21 @@
     <div class="mask undefined undefined" v-on:click="confirm('mask')"></div>
 	<div class="picker" >
 		<div class="header">选择日期</div>
-		<div  class="body">
+		<div  class="body " v-bind:class="{'col-2':type=='normal'}">
 			<div class="mirror"></div>
-			<div ref="year" class="col" style="float: left">
+			<div ref="year" class="col" >
 				<div class="row" id="row1"></div>
 				<div class="row year false" 
 				 v-for="(year,index) in years"  
 				 :class="[{ 'selected': yearvalue === index },randomId]">{{year}}年<span style="font-size: 0px;"></span></div>
 				<div class="row"></div>
 			</div>
-			<div ref="month" class="col" style="float: left">
+			<div ref="month" class="col" >
 				<div class="row"></div>
 				<div class="row month "  v-for="(month,index) in months" :class="[{ 'selected': monthvalue === index ,'false':monthvalue != index},randomId]">{{month}}月<span style="font-size: 0px;"></span></div>
 				<div class="row"></div>
 			</div>	
-			<div ref="day" class="col" style="float: left">
+			<div ref="day" class="col" v-if="type=='full'">
 				<div class="row"></div>
 				<div class="row day "  v-for="(day,index) in days" :class="[{ 'selected': dayvalue === index , 'false':dayvalue != index},randomId]">{{day}}日<span style="font-size: 0px;"></span></div>
 				<div class="row"></div>
@@ -38,6 +38,7 @@
 		name:'paco-picker',
 		props:{
 			value:{type:String},
+			type:{type:String,default:'full'},
 			visible:{type:Boolean},
 			starttime:{type:Number,default:2030},
 			endtime:{type:Number,default:2006},
@@ -141,11 +142,16 @@
 							if(this.yearhight != domyear.scrollTop){
 								if(this.visible){
 									this.yearhight = domyear.scrollTop;
+									if(this.type=="full")
 									this.dates()
 								}	
-								if(dselected.className.indexOf('selected')>0){
-									this.dates()
+								if(this.type=="full"){
+									if(dselected.className.indexOf('selected')>0){
+										if(this.type=="full")
+										this.dates()
+									}									
 								}
+
 
 							}							
 
@@ -165,12 +171,16 @@
 							if(this.monthhight != dommoth.scrollTop){
 								if(this.visible){
 									this.monthhight=dommoth.scrollTop
+									if(this.type=="full")
 									this.dates()
-								}						    
-								if(dselected.className.indexOf('selected')>0){
- 
-									this.dates()
-								}							  				
+								}
+								if(this.type=="full"){
+									if(dselected.className.indexOf('selected')>0){
+ 									
+										this.dates()
+									}									
+								}				    
+							  				
 							}
 						}						
 					}
@@ -195,6 +205,7 @@
 							this.dayvalue = this.setday-1;
 							domyear.scrollTop=this.yearvalue*yselected.clientHeight
 							dommoth.scrollTop=this.monthvalue*mselected.clientHeight
+							if(this.type=="full")
 							domday.scrollTop=this.dayvalue*dselected.clientHeight
 							clearInterval(this.scroll);
 						}						
@@ -220,7 +231,8 @@
 							this.dayvalue = this.setday-1;
 							domyear.scrollTop=this.yearvalue*yselected.clientHeight
 							dommoth.scrollTop=this.monthvalue*mselected.clientHeight
-							domday.scrollTop=this.dayvalue*dselected.clientHeight
+							if(this.type=='full')
+							domday.scrollTop=this.dayvalue*dselected.clientHeight;
 							clearInterval(this.scroll);
 						}						
 					}
